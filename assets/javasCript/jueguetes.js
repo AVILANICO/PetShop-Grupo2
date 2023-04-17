332f6288d3886cb5cbc1ec00e2319de7d2c8ac7a
CrisDev <alvarezmartinez132@gmail.com>
2023-04-17 01:57:40 -0500

    Dinamismo del Modal Con JS

--- assets/javasCript/jueguetes.js
+++ assets/javasCript/jueguetes.js
@@ -1,15 +1,26 @@
 // Referencias
 const $cardsContainer = document.getElementById('cardsContainer')
-let products;
+
 
 // Peticion de data a la API
 fetch( 'https://mindhub-xj03.onrender.com/api/petshop' )
     .then(response => response.json().then(data => {
-        products = data
+        // Asiganacions
+        let products = data
         let filteredPrts = filter(products)
+        const modal = document.getElementById('modalbg')
         printCards(filteredPrts, $cardsContainer)
-        console.log(products)
         
+        let openModal = document.querySelectorAll('.openModal')
+        let arrayHearts = document.querySelectorAll('.heart')
+
+        // Ejecuciones
+        modalEvent(openModal, modal, products, modal)
+        changeHearts(arrayHearts)
+
+
+        console.log(products)
+
     }))
     .catch
 
@@ -24,12 +35,64 @@ function printCards(productArray, container) {
 }
 
 function createCards(obj) {
-    return `        <div class="card margin mx-5 p-3" style="width: 22rem;">
-    <img src="${obj.imagen}" class="card-img-top" alt="...">
-    <h5 class="card-title text-center my-4">${obj.producto}</h5>
-    <div class="card-body text-center d-flex justify-content-center gap-2" style="position: relative; bottom: 0;">
-    <p style="background-color: rgb(199, 47, 47); height: 100%; padding: 10px; border-radius: 5px; color: #fff;">Productos en Stock<span class="ms-2">${obj.disponibles}</span></p>
-    <a href="#" class="btn btn-primary">Mas información</a>
+    return `    <div class="cards d-flex align-items-center"  style="background-image: url(${obj.imagen});">
+                    <p class="titleProduct">${obj.producto}</p>
+                    <div class="none">
+                    <div class="menu">
+                        <i class='bx bx-cart-add cart'></i>
+                        <i class='bx bx-info-circle openModal' id="${obj.producto}"></i>
+                        <i class='bx bxs-heart back heart'></i>
+                    </div>
+                    </div>
+                </div>`
+}
+
+function modalTemplate(obj) {
+    return `<div class="modal_contaniner">
+    <div class="imagen">
+      <i class='bx bx-x d-block text-end xToClose fs-1'></i>
+      <img class="w-100 imgProd" src="${obj.imagen}" alt="">
+    </div>
+    <div class="detail">
+      <h2 class="titleModal">${obj.producto}</h2>
+      <h4 class="text-primary my-4">$${obj.precio}</h4>
+      <h5 class="descriptionTitle mb-4">Descripción</h5>
+      <p class="description">
+      ${obj.descripcion}
+      </p>
+      <div class="compraSection gap-4 my-5">
+        <div class="cantidades d-flex" >
+          <input class="operador buytStl border-end-0" type="button" value="-">
+          <input class="cantidad buytStl text-center" type="number" value="1">
+          <input class="operador buytStl border-start-0" type="button" value="+">
+        </div>
+          <button type="button" class="btn btn-secondary buyBtn w-50">Añadir al carrito</button>
+      </div>
+      <p>En stock: <span>${obj.disponibles}</span></p>
     </div>
   </div>`
 }
+
+function modalEvent(arrayEle, element, arrayData, container){
+    arrayEle.forEach(ele => {
+        ele.addEventListener('click',()=>{
+            element.classList.toggle('mostrarModal')
+            let aux = arrayData.find(prod => prod.producto === ele.id)
+            let toPrint = modalTemplate(aux)
+            container.innerHTML = toPrint
+            let xToCloseModal = document.querySelector('.xToClose')
+            xToCloseModal.addEventListener('click', ()=>{
+                container.classList.toggle('mostrarModal')
+            })
+        })
+    });
+}
+
+function changeHearts(NodeList) {
+    NodeList.forEach(ele => {
+        ele.addEventListener('click', ()=>{
+            ele.classList.toggle('redHeart')
+        })
+    })
+}
+
